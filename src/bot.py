1b109df2d00e0608d786edc5b41f163e7f12f02f
Alex Kouzel <aliaksei.kouzel@gmail.com>
2024-08-13 23:00:51 +0200

    Change 'interval' to 'intervals'

--- src/bot.py
+++ src/bot.py
@@ -93,7 +93,7 @@ I can help you learn and remember words in different languages. By setting up re
 
 To set a new reminder, simply type the word you want to remember directly in the chat.
 
-<b>⏰ To manage interval:</b>
+<b>⏰ To manage intervals:</b>
 /show_intervals - show current intervals
 /set_intervals - set new intervals
 /reset_intervals - reset intervals to default values
